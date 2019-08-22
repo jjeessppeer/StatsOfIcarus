27309307cbfc587c4bba452d4ee1ee753bdcc80f
Jesper Ericsson <jjeessppeer@gmail.com>
2019-08-22 22:19:22 +0200

    added start of gunnery calculator

--- docsapi.js
+++ docsapi.js
@@ -1,11 +1,10 @@
-
+// TODO, add max n return values;
 
 class Dataset {
     constructor(title_array, content_array) {
         this.titles = title_array;
         this.content = content_array;
     }
-
 
     filterByStringMultiCol(query_string, dataset_titles, exclude, case_sensitive) {
         // Return matching 
@@ -20,12 +19,11 @@ class Dataset {
         let result_rows = []
         for (let i = 0; i < this.content.length; i++) {
             for (let j = 0; j < col_idxs.length; j++) {
-                console.log(col_idxs[j]);
-                if (case_sensitive && exclude != this.content[i][col_idxs[j]].includes(query_string)){
+                if (case_sensitive && exclude != this.content[i][col_idxs[j]].includes(query_string)) {
                     result_rows.push(this.content[i]);
                     break;
                 }
-                else if (!case_sensitive && exclude != this.content[i][col_idxs[j]].toLowerCase().includes(query_string.toLowerCase())){
+                else if (!case_sensitive && exclude != this.content[i][col_idxs[j]].toLowerCase().includes(query_string.toLowerCase())) {
                     result_rows.push(this.content[i]);
                     break;
                 }
@@ -35,8 +33,7 @@ class Dataset {
         return new Dataset(this.titles, result_rows);
     }
 
-
-    filterByString(query_string, dataset_title, exclude, case_sensitive) {
+    filterByString(query_string, dataset_title, exclude=false, case_sensitive=false) {
         // Return matching 
         let col_index = this.titles.indexOf(dataset_title);
         if (col_index == -1)
@@ -91,82 +88,117 @@ class Dataset {
 
 }
 
+function loadDatasetFromSheet(sheet_id, sheet_page, load_callback) {
+    let href = "https://spreadsheets.google.com/feeds/cells/" + sheet_id + "/" + sheet_page + "/public/full?alt=json";
+    fetch(href)
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+            let dataset = sheetToDataset(myJson);
+            load_callback(dataset);
+        })
+}
 
-class Sheet {
-    constructor(sheet_id, sheet_page, loaded_callback) {
-        // this.href = "https://spreadsheets.google.com/feeds/list/" + sheet_id + "/default/public/values?alt=json";
-        this.href = "https://spreadsheets.google.com/feeds/cells/" + sheet_id + "/" + sheet_page + "/public/full?alt=json";
-        // https://spreadsheets.google.com/feeds/cells/1Oo1-3ad5_8srmHnc_sUpxgF11kxsapyGs8ogd5cR46g/2/public/full?alt=json
-        this.sheet_json = null;
-        this.dataset = null;
-        fetch(this.href)
-            .then((response) => {
-                return response.json();
-            })
-            .then((myJson) => {
-                // console.log(myJson);
-                this.buildDatabaseArray(myJson);
-                this.sheet_json = myJson;
-            })
-            .then(() => {
-                loaded_callback();
-            });
-    }
+function sheetToDataset(sheet_json) {
+    let entries = sheet_json.feed.entry;
 
-    getDataset() {
-        return this.dataset;
-    }
+    let db_arr = [];
 
-    buildDatabaseArray(sheet_json) {
-        let entries = sheet_json.feed.entry;
-
-        let db_arr = [];
-
-        let last_y = -1;
-        for (let i = 0; i < entries.length; i++) {
-            let coord_text = entries[i].title["$t"];
-            let [x, y] = textToXYCoords(coord_text);
-            if (y != last_y) {
-                // Create new row
-                last_y = y;
-                db_arr.push([]);
-            }
-            // Push column to current row
-            db_arr[last_y].push(entries[i].content["$t"])
-            // console.log(entries[i].content["$t"]);
+    let last_y = -1;
+    for (let i = 0; i < entries.length; i++) {
+        let coord_text = entries[i].title["$t"];
+        let [x, y] = textToXYCoords(coord_text);
+        if (y != last_y) {
+            // Create new row
+            last_y = y;
+            db_arr.push([]);
         }
-
-        this.titles = db_arr[0];
-        db_arr.shift();
-        this.content = db_arr;
-        // console.log(this.titles);
-        // console.log(this.content);
-        this.dataset = new Dataset(this.titles, this.content)
+        // Push column to current row
+        db_arr[last_y].push(entries[i].content["$t"]);
     }
 
-    // getDbRows(){
-    //     return this.content;
-    // }
+    titles = db_arr[0];
+    db_arr.shift();
+    content = db_arr;
+    return new Dataset(titles, content)
+}
 
-    // getDbCell(x, y){
-    //     return this.content[x][y];
-    // }
+// class Sheet {
+//     constructor(sheet_id, sheet_page, loaded_callback) {
+//         // this.href = "https://spreadsheets.google.com/feeds/list/" + sheet_id + "/default/public/values?alt=json";
+//         this.href = "https://spreadsheets.google.com/feeds/cells/" + sheet_id + "/" + sheet_page + "/public/full?alt=json";
+//         // https://spreadsheets.google.com/feeds/cells/1Oo1-3ad5_8srmHnc_sUpxgF11kxsapyGs8ogd5cR46g/2/public/full?alt=json
+//         // this.sheet_json = null;
+//         this.dataset = null;
+//         fetch(this.href)
+//             .then((response) => {
+//                 return response.json();
+//             })
+//             .then((myJson) => {
+//                 // console.log(myJson);
+//                 this.buildDatabaseArray(myJson);
+//                 // this.sheet_json = myJson;
+//             })
+//             .then(() => {
+//                 loaded_callback();
+//             });
+//     }
 
-    // getDbRow(x){
-    //     return this.content[x];
-    // }
+//     getDataset() {
+//         return this.dataset;
+//     }
 
-    // getDbRowsByString(query_string, query_title){
-    //     // Return matching 
-    //     let col_index = this.titles.indexOf(query_title);
-    //     if (col_index == -1)
-    //         return [];
-    //     let result_rows = []
-    //     for(let i=0; i<this.content.length; i++){
-    //         if (this.content[i][col_index].includes(query_string))
-    //             result_rows.push(this.content[i]);
-    //     }
-    //     return result_rows;
-    // }
+//     buildDatabaseArray(sheet_json) {
+//         let entries = sheet_json.feed.entry;
 
-};
+//         let db_arr = [];
+
+//         let last_y = -1;
+//         for (let i = 0; i < entries.length; i++) {
+//             let coord_text = entries[i].title["$t"];
+//             let [x, y] = textToXYCoords(coord_text);
+//             if (y != last_y) {
+//                 // Create new row
+//                 last_y = y;
+//                 db_arr.push([]);
+//             }
+//             // Push column to current row
+//             db_arr[last_y].push(entries[i].content["$t"])
+//             // console.log(entries[i].content["$t"]);
+//         }
+
+//         this.titles = db_arr[0];
+//         db_arr.shift();
+//         this.content = db_arr;
+//         // console.log(this.titles);
+//         // console.log(this.content);
+//         this.dataset = new Dataset(this.titles, this.content)
+//     }
+
+//     // getDbRows(){
+//     //     return this.content;
+//     // }
+
+//     // getDbCell(x, y){
+//     //     return this.content[x][y];
+//     // }
+
+//     // getDbRow(x){
+//     //     return this.content[x];
+//     // }
+
+//     // getDbRowsByString(query_string, query_title){
+//     //     // Return matching 
+//     //     let col_index = this.titles.indexOf(query_title);
+//     //     if (col_index == -1)
+//     //         return [];
+//     //     let result_rows = []
+//     //     for(let i=0; i<this.content.length; i++){
+//     //         if (this.content[i][col_index].includes(query_string))
+//     //             result_rows.push(this.content[i]);
+//     //     }
+//     //     return result_rows;
+//     // }
+
+// };
