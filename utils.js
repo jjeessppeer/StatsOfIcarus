a9163e237137b2c10f05a91a4aac5f0838711090
Jesper Ericsson <jjeessppeer@gmail.com>
2019-08-24 19:50:04 +0200

    added start of arc calculator

--- utils.js
+++ utils.js
@@ -25,8 +25,6 @@ function parseDate(text){
         return new Date(year, month, day);
     }
     return new Date(year, month);
-    if(text)
-    return false;
 }
 
 function precise(x, digits){
@@ -34,7 +32,105 @@ function precise(x, digits){
     return parseFloat(Number.parseFloat(x).toPrecision(digits));
 }
 
-
 function getDamageMod(damageType, target){
     return damage_dataset.getCellByString(damageType, "Name", target)
-  }
+}
+
+
+function getProjectileArc(speed, angle, start_height, drop, start, end, steps){
+    
+
+    let points = [];
+    for (let i=0; i<steps; i++){
+        let t = start + (end - start) * i / steps;
+        points.push(projectilePos(t, speed, angle, drop, start_height));
+    }
+    return points;
+}
+
+function searchAngle(speed, start_height, drop, target_point){
+    let y;
+    let angle = - Math.PI / 2;
+    let angle_step = Math.PI / 100;
+    let max_loops = Math.PI / angle_step;
+    let loops = 0;
+
+    
+    // First find out if target is reachable
+    do{
+        if (loops > max_loops){
+            console.log("hit not found");
+            break;
+        }
+        loops++;
+        
+        let t = tAtHit(speed, angle, target_point[0]);
+        y = projectilePos(t, speed, angle, drop, start_height)[1];
+        angle += angle_step;
+        // console.log("Testing angle: ", precise(angle/Math.PI,3), " : ", precise(y, 3), " : ", precise(target_point[1], 3))
+        
+    }while(y < target_point[1]);
+    angle -= angle_step;
+    console.log(angle)
+    console.log(angle-angle_step)
+
+    console.log("Angle found: ", angle, " : ", precise(y, 3), " : ", precise(target_point[1], 3));
+    console.log("Angle found: ", (angle-angle_step), " : ", precise(y, 3), " : ", precise(target_point[1], 3));
+    // Refine for 10 iterations
+    let a1 = angle;
+    let a2 = angle - angle_step;
+    let a3 = (a1 + a2) / 2;
+
+    for (i=0; i<5; i++){
+        let a3 = (a1 + a2) / 2;
+        let t1 = tAtHit(speed, a1, target_point[0]);
+        let t2 = tAtHit(speed, a2, target_point[0]);
+        let t3 = tAtHit(speed, a3, target_point[0]);
+
+
+        let d1 = projectilePos(t1, speed, a1, drop, start_height)[1] - target_point[1];
+        let d2 = projectilePos(t2, speed, a2, drop, start_height)[1] - target_point[1];
+        let d3 = projectilePos(t3, speed, a3, drop, start_height)[1] - target_point[1];
+        
+        // console.log("a3: ", a3);
+        // console.log
+        console.log("\nHalving iteration");
+        // console.log("a1", a1);
+        // console.log("a2", a2);
+        // console.log("a3", a3);
+        console.log("Diff")
+        console.log(d1);
+        console.log(d2);
+        console.log(d3);
+
+
+        if (d3 > 0 && d3 < d1) a1 = a3;
+        else if (d3 < 0 && d3 > d2) a2 = a3;
+        else{
+            console.log("Middle angle worst");
+            break;
+        }
+    }
+
+    let t = tAtHit(speed, a1, target_point[0]);
+    y = projectilePos(t, speed, a1, drop, start_height)[1];
+
+    console.log("Angle refined: ", a1, " : ", y, " : ", precise(target_point[1], 3));
+
+
+
+    return getProjectileArc(speed, a1, start_height, drop, 0, tAtHit(speed, a1, target_point[0]), 100);
+
+
+}
+
+// trigonometry yo
+
+function projectilePos(t, speed, angle, drop, start_height){
+    return [speed * Math.cos(angle) * t,
+            speed * Math.sin(angle) * t - drop/2 * Math.pow(t, 2) + start_height];
+}
+
+function tAtHit(speed, angle, target_x){
+    return target_x/(speed * Math.cos(angle));
+}
