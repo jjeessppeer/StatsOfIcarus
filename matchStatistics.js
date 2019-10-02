7c62a09dd088357630fa48583196aa8a5426d8c9
Jesper Ericsson <jjeessppeer@gmail.com>
2019-10-02 17:17:32 +0200

    words

--- matchStatistics.js
+++ matchStatistics.js
@@ -273,7 +273,7 @@ function updateCharts(wins, losses, pick_data, enemy_data, ally_data, scatter){
                 },
                 title: {
                     display: true,
-                    text: 'Pick rate per team'
+                    text: 'Pick rate'
                 },
                 animation: {
                     animateScale: true,
