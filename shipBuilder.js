d899ff963fa458553f36d33e3799fb42bc125db5
Jesper Ericsson <jjeessppeer@gmail.com>
2019-10-28 10:28:00 +0100

    ship builder gun selection

--- shipBuilder.js
+++ shipBuilder.js
@@ -1,35 +1,60 @@
 
 
-var selected_guns = ["None", "None", "None", "None", "None", "None"];
+var ship_builder_guns = ["None", "None", "None", "None", "None", "None"];
 
 function initializeShipBuilder(){
 
-  updateShipBuilder();
+
+
+  shipBuilderShipChanged();
 
   updateShipBuildImage();
   updateRangeVis();
-
+  console.log("GUNS INITIALZIED");
 
   $('#shipBuilderCanvas').mousedown(shipCanvasClicked);
+
+  $("#weaponSelections select").on("change", function(e){
+    ship_builder_guns[$(this).parent().index()/2] = $(this).val();
+    console.log(ship_builder_guns);
+    updateShipBuildImage();
+  });
 }
 
 
-function updateShipBuilder(){
+function shipBuilderShipChanged(){
   if (!(gun_dataset && ammo_dataset && ship_dataset && ship_guns_dataset && map_dataset)) {
     console.log("Still loading");
-    setTimeout(function(){ updateShipBuilder(); }, 1000);
+    setTimeout(function(){ shipBuilderShipChanged(); }, 1000);
     return;
   }
-
-
-
+  ship_builder_guns.fill("None");
+  
+  let ship_data = ship_guns_dataset.filterByString("Mobula", "Ship").getDatasetRow(0);
+  let n_guns = parseInt(ship_data[1]);
+  console.log(n_guns);
+  for (let i=0; i < n_guns; i++){
+    
+    console.log(i);
+    let available_guns = gun_dataset.filterByString(ship_data[14+i], "Weapon slot");
+    let select = $("#weaponSelections > div:nth-of-type("+(i+1)+") > select");
+    select.empty();
+    for (let j=0; j < available_guns.getNOfRows(); j++){
+      select.append($("<option>"+available_guns.getDatasetCell(j, 1)+"</option>"));      
+    }
+    ship_builder_guns[i] = select.val();
+  }
+  console.log(ship_builder_guns)
 }
+
+
+
+
 
 
 function shipCanvasClicked(event){
   if(!event) event = window.event;  
   
-
   let click_pos = [event.pageX - $(this).offset().left, event.pageY - $(this).offset().top];
   var x = event.pageX - $(this).offset().left;  
   var y = event.pageY - $(this).offset().top;   
@@ -46,7 +71,7 @@ function shipCanvasClicked(event){
   for (let i=0; i < n_guns; i++){
     if (dist2D([x, y], pointStringToInts(data_row[8+i])) < 10) gun_i = i; 
   }
-  console.log(gun_i);
+  console.log("N guns: ", gun_i);
 
 
   let available_guns = gun_dataset.filterByString(data_row[14+gun_i], "Weapon slot");
@@ -85,20 +110,20 @@ function updateShipBuildImage(){
 
   let canvas = document.getElementById("shipBuilderCanvas");
   let ctx = canvas.getContext("2d");
-
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   
   let data_row = ship_guns_dataset.filterByString("Mobula", "Ship").getDatasetRow(0);
-  
-
-  
 
   let n_guns = parseInt(data_row[1]);
 
   for (let i=0; i < n_guns; i++){
 
+    let gun_type = ship_builder_guns[i];
+    let gun_angle = parseFloat(gun_dataset.getCellByString(gun_type, "Alias", "Side angle"));
+
     let angle = degToRad(90 + parseInt(data_row[2+i]));
-    let right_angle = angle + degToRad(60);
-    let left_angle = angle - degToRad(60);
+    let right_angle = angle + degToRad(gun_angle);
+    let left_angle = angle - degToRad(gun_angle);
     // let [cx, cy] = 100;
 
 
@@ -126,11 +151,6 @@ function updateShipBuildImage(){
 
 }
 
-
-
-
-
-
 function updateRangeVis(){
     if (!(gun_dataset && ammo_dataset && ship_dataset && map_dataset)) {
       console.log("Still loading");
@@ -148,6 +168,18 @@ function updateRangeVis(){
     let map_image = document.querySelector("#mapImage");
     let map_scale = map_dataset.getCellByString($("#arcMapSelect").val(), "Name", "Map scale (m/px)") / (map_image.width / map_image.naturalWidth);
     
+
+
+    let ship_data = ship_guns_dataset.filterByString("Mobula", "Ship").getDatasetRow(0);
+    let n_guns = parseInt(ship_data[1]);
+
+    // for (let i=0; i < ship_builder_guns.length; i++){
+    //   let gun_type = ship_builder_guns[i];
+    //   let ammo_type = "Charged";
+
+    // }
+
+
     let gun_type = "Lumberjack";
     let ammo_type = "Charged";
     
