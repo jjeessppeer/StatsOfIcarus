6527364d73d30a8e52afa79383c166a2b974e202
Jesper Ericsson <jjeessppeer@gmail.com>
2020-02-27 00:30:43 +0100

    stat database can now be updatde from google

---
+++ docsapi.js
@@ -0,0 +1,207 @@
+const fetch = require('node-fetch');
+
+class Dataset {
+    constructor(title_array, content_array) {
+        this.titles = title_array;
+        this.content = content_array;
+        this.sanitizeDataset();
+    }
+
+    sanitizeDataset(){
+        for (let i=0; i<this.titles.length; i++){
+            this.titles[i] = sanitizeHtml(this.titles[i])
+        }
+        for (let i=0; i<this.content.length; i++){
+            for (let j=0; j<this.content[i].length; j++){
+                this.content[i][j] = sanitizeHtml(this.content[i][j])
+            }
+        }
+    }
+
+    filterByStringMultiCol(query_string, dataset_titles, exclude, case_sensitive) {
+        // Return matching 
+        let col_idxs = [];
+        for (let i = 0; i < dataset_titles.length; i++) {
+            let col_index = this.titles.indexOf(dataset_titles[i]);
+            if (col_index == -1)
+                return new Dataset(this.titles, []);
+            col_idxs.push(col_index);
+        }
+
+        let result_rows = []
+        for (let i = 0; i < this.content.length; i++) {
+            for (let j = 0; j < col_idxs.length; j++) {
+                if (case_sensitive && exclude != this.content[i][col_idxs[j]].includes(query_string)) {
+                    result_rows.push(this.content[i]);
+                    break;
+                }
+                else if (!case_sensitive && exclude != this.content[i][col_idxs[j]].toLowerCase().includes(query_string.toLowerCase())) {
+                    result_rows.push(this.content[i]);
+                    break;
+                }
+            }
+
+        }
+        return new Dataset(this.titles, result_rows);
+    }
+
+    filterByString(query_string, dataset_title, exclude=false, case_sensitive=false) {
+        // Return matching 
+        let col_index = this.titles.indexOf(dataset_title);
+        if (col_index == -1)
+            return new Dataset(this.titles, []);
+        let result_rows = []
+        for (let i = 0; i < this.content.length; i++) {
+            if (case_sensitive && exclude != this.content[i][col_index].includes(query_string))
+                result_rows.push(this.content[i]);
+            else if (!case_sensitive && exclude != this.content[i][col_index].toLowerCase().includes(query_string.toLowerCase()))
+                result_rows.push(this.content[i]);
+        }
+        return new Dataset(this.titles, result_rows);
+    }
+
+    filterByDate(query, dataset_title, exclude, after) {
+        // let query_date = parseDate(query);
+        let query_date = new Date(Date.parse(query));
+        if (!query_date) {
+            return new Dataset(this.titles, this.content);
+        }
+        let col_index = this.titles.indexOf(dataset_title);
+        if (col_index == -1)
+            return new Dataset(this.titles, []);
+        let result_rows = []
+        for (let i = 0; i < this.content.length; i++) {
+            let db_date = new Date(this.content[i][col_index]);
+            if (exclude != (after == (query_date <= db_date)))
+                result_rows.push(this.content[i]);
+
+        }
+        return new Dataset(this.titles, result_rows);
+    }
+
+    filterByRow(start_row, end_row=start_row){
+        let result_rows = [];
+        for (var i = start_row; i <= end_row; i++) {
+            result_rows.push(this.content[i]);
+        }
+        return new Dataset(this.titles, result_rows);
+    }
+
+    sortDatasetContent(sort_title, valueFunction){
+        let col_index = this.titles.indexOf(sort_title);
+        if (col_index == -1)
+            return false;
+        this.content.sort(function(x, y){
+            if (valueFunction(x[col_index]) < valueFunction(y[col_index])){
+                return 1;
+            }
+            if (valueFunction(x[col_index]) > valueFunction(y[col_index])){
+                return -1;
+            }
+            return 0;
+        });
+    }
+
+    getCellByString(query_string, search_title, return_title){
+        // Return first cell matching query and title, if multiple matches exist returns top one.
+        let col_index = this.titles.indexOf(return_title);
+        if (col_index == -1)
+            return false;
+        return this.filterByString(query_string, search_title).getDatasetRow(0)[col_index];
+    }
+
+    getNOfRows(){
+        return this.content.length;
+    }
+
+    getDatasetRows() {
+        return this.content;
+    }
+
+    getDatasetRow(x) {
+        return this.content[x];
+    }
+
+    getDatasetCell(y, x) {
+        return this.content[y][x];
+    }
+
+    getFirstCellByTitle(return_title){
+        let col_index = this.titles.indexOf(return_title);
+        if (col_index == -1)
+            return false;
+        return this.getDatasetCell(0, col_index);
+    }
+
+    getEmptyContent() {
+        return new Dataset(this.titles, []);
+    }
+
+    mergeDataset(otherDataset) {
+        return new Dataset(this.titles, this.content.concat(otherDataset.content));
+    }
+
+}
+
+function loadDatasetFromSheet(sheet_id, sheet_page, load_callback) {
+    let href = "https://spreadsheets.google.com/feeds/cells/" + sheet_id + "/" + sheet_page + "/public/full?alt=json";
+    fetch(href)
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+            // console.log(myJson);
+            let dataset = sheetToDataset(myJson);
+            load_callback(dataset);
+        })
+}
+
+function sheetToDataset(sheet_json) {
+    let entries = sheet_json.feed.entry;
+
+    let db_arr = [];
+
+    let last_y = -1;
+    for (let i = 0; i < entries.length; i++) {
+        let y = parseInt(entries[i]["gs$cell"].row) - 1;
+        let x = parseInt(entries[i]["gs$cell"].col) - 1;
+        // console.log("x: ", x, ", y: ", y);
+        if (y != last_y) {
+            // Create new row
+            last_y = y;
+            db_arr.push([]);
+        }
+        // Push column to current row
+        db_arr[last_y].push(entries[i]["gs$cell"].inputValue);
+    }
+
+    titles = db_arr[0];
+    db_arr.shift();
+    content = db_arr;
+    return new Dataset(titles, content)
+}
+
+function jsonToDataset(dataset_arr, titles){
+    let content = [];
+    // console.log(dataset_arr);
+    for (let i = 0; i < dataset_arr.length; i++) {
+        let row = [];
+        for (let j=0; j<titles.length; j++){
+            // console.log(titles[j].replace(" ", ""));
+            row.push(dataset_arr[i][titles[j].replace(/ /g, "").replace(",", "")]);
+        }
+        content.push(row);
+    }
+    return new Dataset(titles, content);
+}
+
+function sanitizeHtml(str){
+    str = String(str);
+    return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
+
+module.exports = {
+    Dataset: Dataset,
+    loadDatasetFromSheet: loadDatasetFromSheet
+};
---
+++ pull_database.js
@@ -0,0 +1,70 @@
+const sqlite = require('better-sqlite3');
+const data_db = new sqlite('data_db.db', { verbose: null });
+
+//var docsapi = require('./public/docsapi.js');
+
+var docsapi = require('./docsapi.js');
+
+const sheet_id = "1Oo1-3ad5_8srmHnc_sUpxgF11kxsapyGs8ogd5cR46g";
+
+
+var datasets = [];
+
+
+data_db.prepare('DELETE FROM Ammo_stats;').run();
+data_db.prepare('DELETE FROM Component_stats;').run();
+data_db.prepare('DELETE FROM Damage_types;').run();
+data_db.prepare('DELETE FROM Gun_stats;').run();
+data_db.prepare('DELETE FROM Map_data;').run();
+data_db.prepare('DELETE FROM Match_History;').run();
+data_db.prepare('DELETE FROM Ship_Stats;').run();
+data_db.prepare('DELETE FROM Ships_gun_angles;').run();
+data_db.prepare('DELETE FROM Tool_stats;').run();
+data_db.prepare('DELETE FROM Crosshair_data;').run();
+
+
+docsapi.loadDatasetFromSheet(sheet_id, 2, (dataset) => {parseDataset(dataset, "Match_History")});
+docsapi.loadDatasetFromSheet(sheet_id, 4, (dataset) => {parseDataset(dataset, "Gun_stats")});
+docsapi.loadDatasetFromSheet(sheet_id, 5, (dataset) => {parseDataset(dataset, "Ammo_stats")});
+docsapi.loadDatasetFromSheet(sheet_id, 6, (dataset) => {parseDataset(dataset, "Damage_types")});
+docsapi.loadDatasetFromSheet(sheet_id, 7, (dataset) => {parseDataset(dataset, "Tool_stats")});
+docsapi.loadDatasetFromSheet(sheet_id, 8, (dataset) => {parseDataset(dataset, "Component_stats")});
+docsapi.loadDatasetFromSheet(sheet_id, 9, (dataset) => {parseDataset(dataset, "Ship_Stats")});
+docsapi.loadDatasetFromSheet(sheet_id, 10, (dataset) => {parseDataset(dataset, "Map_data")});
+docsapi.loadDatasetFromSheet(sheet_id, 11, (dataset) => {parseDataset(dataset, "Crosshair_data")});
+docsapi.loadDatasetFromSheet(sheet_id, 12, (dataset) => {parseDataset(dataset, "Ships_gun_angles")});
+
+function parseDataset(dataset, table_name){
+  let cmd = "INSERT INTO " + table_name + " VALUES (";
+  for (let i=0; i < dataset.titles.length; i++){
+    cmd += "?";
+    if (i != dataset.titles.length-1) cmd+=",";
+  }
+  cmd += ")";
+
+  for (let i=0; i < dataset.content.length; i++){
+    data_db.prepare(cmd).run(dataset.content[i]);
+
+  }
+
+  
+}
+
+// Clear database
+
+/*
+data_db.prepare('DROP TABLE Ammo_stats');
+data_db.prepare('DROP TABLE Component_stats');
+data_db.prepare('DROP TABLE Damage_types');
+data_db.prepare('DROP TABLE Gun_stats');
+data_db.prepare('DROP TABLE Map_data');
+data_db.prepare('DROP TABLE Match_History');
+data_db.prepare('DROP TABLE Ship_Stats');
+data_db.prepare('DROP TABLE Ships_gun_angles');
+data_db.prepare('DROP TABLE Tool_stats');
+
+// Recreate tables
+*/
+
+
+
