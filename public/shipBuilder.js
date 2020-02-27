d6e81d31c4de059a91d8a8e406528cf9e08aab54
Jesper Ericsson <jjeessppeer@gmail.com>
2020-02-27 16:57:07 +0100

    improved dark mode build visualisation

--- public/shipBuilder.js
+++ public/shipBuilder.js
@@ -576,6 +576,19 @@ function updateShipBuildImage(){
   ctx.globalAlpha = 1;
   ctx.drawImage(img, 0, 0);
 
+
+  if ($("#darkModeSwitch")[0].checked) {
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let data = imageData.data;
+    for (let i=0; i < data.length; i+=4){
+      data[i] = 255-data[i];
+      data[i+1] = 255-data[i+1];
+      data[i+2] = 255-data[i+2];
+    }
+    ctx.putImageData(imageData, 0, 0);
+  }
+  
+
   let data_row = ship_guns_dataset.filterByString(ship_builder_ship, "Ship").getDatasetRow(0);
 
   let n_guns = parseInt(data_row[1]);
