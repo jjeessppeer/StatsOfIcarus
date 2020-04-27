06a5d7ce160070fd728c60ea99e6d0eda5472520
Jesper Ericsson <jjeessppeer@gmail.com>
2020-04-27 17:12:16 +0200

    new map database structure

--- pullDatabase.js
+++ pullDatabase.js
@@ -7,22 +7,13 @@ var docsapi = require('./docsapi.js');
 
 const sheet_id = "1Oo1-3ad5_8srmHnc_sUpxgF11kxsapyGs8ogd5cR46g";
 
-
 var datasets = [];
 
 
-data_db.prepare('DELETE FROM Ammo_stats;').run();
-data_db.prepare('DELETE FROM Component_stats;').run();
-data_db.prepare('DELETE FROM Damage_types;').run();
-data_db.prepare('DELETE FROM Gun_stats;').run();
-data_db.prepare('DELETE FROM Map_data;').run();
-data_db.prepare('DELETE FROM Match_History;').run();
-data_db.prepare('DELETE FROM Ship_Stats;').run();
-data_db.prepare('DELETE FROM Ships_gun_angles;').run();
-data_db.prepare('DELETE FROM Tool_stats;').run();
-data_db.prepare('DELETE FROM Crosshair_data;').run();
+dropTables();
+initTables();
 
-
+console.log("Downloading datasets...");
 //docsapi.loadDatasetFromSheet(sheet_id, 2, (dataset) => {parseDataset(dataset, "Match_History")});
 docsapi.loadDatasetFromSheet(sheet_id, 4, (dataset) => {parseDataset(dataset, "Gun_stats")});
 docsapi.loadDatasetFromSheet(sheet_id, 5, (dataset) => {parseDataset(dataset, "Ammo_stats")});
@@ -45,10 +36,158 @@ function parseDataset(dataset, table_name){
   for (let i=0; i < dataset.content.length; i++){
     data_db.prepare(cmd).run(dataset.content[i]);
 
-  }
-
-  
+  } 
 }
+
+function dropTables(){
+  data_db.prepare("DROP TABLE Ammo_stats").run();
+  data_db.prepare("DROP TABLE Component_stats").run();
+  data_db.prepare("DROP TABLE Damage_types").run();
+  data_db.prepare("DROP TABLE Gun_stats").run();
+  data_db.prepare("DROP TABLE Map_data").run();
+  data_db.prepare("DROP TABLE Match_History").run();
+  data_db.prepare("DROP TABLE Ship_Stats").run();
+  data_db.prepare("DROP TABLE Ships_gun_angles").run();
+  data_db.prepare("DROP TABLE Tool_stats").run();
+  data_db.prepare("DROP TABLE Crosshair_data").run();
+  console.log("Tables cleared.")
+}
+
+function initTables(){
+  data_db.prepare(`CREATE TABLE "Ammo_stats" (
+    "Name"	TEXT,
+    "Alias"	TEXT,
+    "Rotationspeed"	INTEGER,
+    "Jitter"	INTEGER,
+    "Clipsize"	INTEGER,
+    "AOEradius"	INTEGER,
+    "AOEdamage"	INTEGER,
+    "Damage"	INTEGER,
+    "Armingtime"	INTEGER,
+    "Rateoffire"	INTEGER,
+    "Projectilespeed"	INTEGER,
+    "Firemodifier"	INTEGER,
+    "Firedamage"	INTEGER,
+    "Lift"	INTEGER,
+    "Directdamage"	INTEGER,
+    "Range"	INTEGER,
+    "Rotationalarcs"	INTEGER
+  )`).run();
+  data_db.prepare(`CREATE TABLE "Component_stats" (
+    "Name"	TEXT,
+    "HP"	INTEGER
+  )`).run();
+  data_db.prepare(`CREATE TABLE "Crosshair_data" (
+    "Name"	TEXT,
+    "Degperpixel"	INTEGER,
+    "Starty"	INTEGER,
+    "Startx"	INTEGER,
+    "Endx"	INTEGER,
+    "Image"	TEXT
+  )`).run();
+  data_db.prepare(`CREATE TABLE "Damage_types" (
+    "Name"	TEXT,
+    "Balloon"	INTEGER,
+    "Hull"	INTEGER,
+    "Armor"	INTEGER,
+    "Components"	INTEGER
+  )`).run();
+  data_db.prepare(`CREATE TABLE "Gun_stats" (
+    "Name"	TEXT,
+    "Alias"	TEXT,
+    "Mode"	TEXT,
+    "Weaponslot"	TEXT,
+    "Damagetypeprimary"	TEXT,
+    "Damageprimary"	INTEGER,
+    "Damagetypesecondary"	TEXT,
+    "Damagesecondary"	INTEGER,
+    "Rateoffire"	INTEGER,
+    "Reloadtime"	INTEGER,
+    "Magazinesize"	INTEGER,
+    "Fireprimary"	INTEGER,
+    "Firesecondary"	INTEGER,
+    "Projectilespeed"	INTEGER,
+    "Range"	INTEGER,
+    "Shelldrop"	INTEGER,
+    "AOEradius"	INTEGER,
+    "Buckshot"	INTEGER,
+    "Armingtime"	REAL,
+    "Sideangle"	INTEGER,
+    "Upangle"	INTEGER,
+    "Downangle"	INTEGER
+  )`).run();
+  data_db.prepare(`CREATE TABLE "Map_data" (
+    "ID" TEXT,
+    "Name"	TEXT,
+    "Mode"	TEXT,
+    "Size"	TEXT,
+    "MapImage"	TEXT,
+    "SpawnImage"	TEXT,
+    "Mapscale"	TEXT
+  )`).run();
+  data_db.prepare(`CREATE TABLE "Match_history" (
+    "Timestamp"	TEXT,
+    "Dateofmatch"	TEXT,
+    "Event"	TEXT,
+    "Teamscores[Team1score]"	INTEGER,
+    "Teamscores[Team2score]"	INTEGER,
+    "T1Ship1"	TEXT,
+    "T1Ship2"	TEXT,
+    "T2Ship1"	TEXT,
+    "T2Ship2"	TEXT,
+    "T1S1Pilot"	TEXT,
+    "T1S2Pilot"	INTEGER,
+    "T2S1Pilot"	TEXT,
+    "T2S2Pilot"	TEXT
+  )`).run();
+  data_db.prepare(`CREATE TABLE "Ship_Stats" (
+    "ShipType"	TEXT,
+    "Armor"	INTEGER,
+    "ArmorRebuildValue"	INTEGER,
+    "HullHealth"	INTEGER,
+    "LightWeapons"	INTEGER,
+    "HeavyWeapons"	INTEGER,
+    "LongitudinalSpeed(m/s)"	INTEGER,
+    "LongitudinalAcceleration(m/s²)"	INTEGER,
+    "TurnSpeed(deg/s)"	INTEGER,
+    "TurnAcceleration(deg/s²)"	INTEGER,
+    "VerticalSpeed(m/s)"	INTEGER,
+    "VerticalAcceleration(m/s²)"	INTEGER,
+    "Mass(t)"	INTEGER
+  )`).run();
+  data_db.prepare(`CREATE TABLE "Ships_gun_angles" (
+    "Ship"	TEXT,
+    "Nguns"	INTEGER,
+    "Angle1"	REAL,
+    "Angle2"	INTEGER,
+    "Angle3"	INTEGER,
+    "Angle4"	TEXT,
+    "Angle5"	TEXT,
+    "Angle6"	TEXT,
+    "Position1"	TEXT,
+    "Position2"	TEXT,
+    "Position3"	TEXT,
+    "Position4"	TEXT,
+    "Position5"	TEXT,
+    "Position6"	TEXT,
+    "Slot1"	TEXT,
+    "Slot2"	TEXT,
+    "Slot3"	TEXT,
+    "Slot4"	TEXT,
+    "Slot5"	TEXT,
+    "Slot6"	TEXT
+  )`).run();
+  data_db.prepare(`CREATE TABLE "Tool_stats" (
+    "Name"	TEXT,
+    "Repair"	INTEGER,
+    "Rebuild"	INTEGER,
+    "Extinguish"	INTEGER,
+    "Fireimmunity"	INTEGER,
+    "Cooldown"	INTEGER
+  )`).run();
+  console.log("Tables initialized.")
+}
+
 
 // Clear database
 
