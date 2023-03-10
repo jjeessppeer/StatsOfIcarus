b93c2ea5cb0be50db2282a7c097c8452eaf386d2
Jesper Ericsson <jjeessppeer@gmail.com>
2023-03-10 10:23:40 +0100

    removed sqlite dependencies, moved to mongodb

--- main.js
+++ main.js
@@ -15,15 +15,12 @@ const {HISTORY_SEARCH_SCHEMA, MATCH_REQUEST_SCHEMA, MATCH_SUBMISSION_SCHEMA, PLA
 const db_url = process.env.MONGODB_URL_STRING;
 let mongoClient = new MongoClient(db_url);
 
-const MOD_VERSION_LATEST = "0.1.3";
+const MOD_VERSION_LATEST = "0.2.0";
 
 // var bodyParser = require("body-parser");
 var requestIp = require('request-ip');
-const sqlite = require('better-sqlite3');
 const { assert } = require('console');
 const { nextTick } = require('process');
-
-const data_db = new sqlite('databases/data_db.db', { fileMustExist: true, readonly: true });
 
 var app = express()
 
@@ -32,20 +29,26 @@ var app = express()
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/get_datasets', function (req, res) {
+app.get('/get_datasets', async function (req, res) {
     let ip = requestIp.getClientIp(req);
+    const db = mongoClient.db("GameStats");
     let datasets = {};
-    // datasets.history = data_db.prepare("SELECT * FROM Match_History").all();
-    datasets.gun_stats = data_db.prepare("SELECT * FROM Gun_stats").all();
-    datasets.ammo_stats = data_db.prepare("SELECT * FROM Ammo_stats").all();
-    datasets.damage_stats = data_db.prepare("SELECT * FROM Damage_types").all();
-    datasets.tool_stats = data_db.prepare("SELECT * FROM Tool_stats").all();
-    datasets.component_stats = data_db.prepare("SELECT * FROM Component_stats").all();
-    datasets.ship_stats = data_db.prepare("SELECT * FROM Ship_Stats").all();
-    datasets.map_data = data_db.prepare("SELECT * FROM Map_data").all();
-    datasets.crosshair_data = data_db.prepare("SELECT * FROM Crosshair_data").all();
-    datasets.ships_gun_angles = data_db.prepare("SELECT * FROM Ships_gun_angles").all();
-    res.status("200").json(datasets);
+    datasets.gun_stats = await db.collection("Gun_stats").find({}).toArray();
+    datasets.ammo_stats = await db.collection("Ammo_stats").find({}).toArray();
+    datasets.damage_stats = await db.collection("Damage_types").find({}).toArray();
+    datasets.tool_stats = await db.collection("Tool_stats").find({}).toArray();
+    datasets.component_stats = await db.collection("Component_stats").find({}).toArray();
+    datasets.ship_stats = await db.collection("Ship_Stats").find({}).toArray();
+    datasets.map_data = await db.collection("Map_data").find({}).toArray();
+    datasets.crosshair_data = await db.collection("Crosshair_data").find({}).toArray();
+    datasets.ships_gun_angles = await db.collection("Ships_gun_angles").find({}).toArray();
+    for (const [key, value] of Object.entries(datasets)) {
+        for (const v of datasets[key]) {
+            delete v._id;
+        }
+    }
+
+    res.status(200).json(datasets);
 });
 
 app.post('/submit_match_history', async function (req, res) {
