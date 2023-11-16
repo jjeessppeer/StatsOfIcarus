87a74f6bfcfcd2a5ef0a21799c78342e6c4e317b
Jesper Ericsson <jjeessppeer@gmail.com>
2023-11-16 13:23:00 +0100

    Stop using sqlite, moved schemas to new file

--- Server/main.js
+++ Server/main.js
@@ -17,45 +17,55 @@ const matchHistoryRetrieve = require("./MatchHistory/matchHistoryRetrieve.js");
 const matchHistoryUtils = require("./MatchHistory/matchHistoryUtils.js");
 const elo = require("./Elo/EloHelper.js");
 const lobbyBalancer = require("./Elo/LobbyBalancer.js");
-const {HISTORY_SEARCH_SCHEMA, MATCH_REQUEST_SCHEMA, MATCH_SUBMISSION_SCHEMA, PLAYER_SUBMISSION_SCHEMA} = require("./MatchHistory/requestSchemas.js");
-
+const {HISTORY_SEARCH_SCHEMA, MATCH_REQUEST_SCHEMA, MATCH_SUBMISSION_SCHEMA} = require("./MatchHistory/requestSchemas.js");
+const { MATCH_SUBMISSION_SCHEMA } = require("./SchemaValidation/Schemas/HistorySubmission.js");
 
 const shipStats = require('./ShipStats/ShipStats.js');
 
 const { MONGODB_URL_STRING } = require("../config.json");
 let mongoClient = new MongoClient(MONGODB_URL_STRING);
 
-const MOD_VERSION_LATEST = "0.1.3";
+const MOD_VERSION_LATEST = "0.2.0";
 const MOD_VERSION_REQUIRED = "0.1.3";
+
+const zlib = require('zlib');
+const util = require('util');
+const unzip = util.promisify(zlib.unzip);
+const inflate = util.promisify(zlib.inflate);
 
 // var bodyParser = require("body-parser");
 var requestIp = require('request-ip');
-const sqlite = require('better-sqlite3');
 const { assert } = require('console');
 const { nextTick } = require('process');
 
-const data_db = new sqlite('databases/data_db.db', { fileMustExist: true, readonly: true });
-
 var app = express()
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.text({ limit: '100mb' }));
+app.use(express.urlencoded({
+    limit: '100mb', 
+    extended: true}));
 app.use(express.static('public'));
 
-app.get('/get_datasets', function (req, res) {
+app.get('/get_datasets', async function (req, res) {
     let ip = requestIp.getClientIp(req);
+    const db = mongoClient.db("testdb1");
     let datasets = {};
-    // datasets.history = data_db.prepare("SELECT * FROM Match_History").all();
-    datasets.gun_stats = data_db.prepare("SELECT * FROM Gun_stats").all();
-    datasets.ammo_stats = data_db.prepare("SELECT * FROM Ammo_stats").all();
-    datasets.damage_stats = data_db.prepare("SELECT * FROM Damage_types").all();
-    datasets.tool_stats = data_db.prepare("SELECT * FROM Tool_stats").all();
-    datasets.component_stats = data_db.prepare("SELECT * FROM Component_stats").all();
-    datasets.ship_stats = data_db.prepare("SELECT * FROM Ship_Stats").all();
-    datasets.map_data = data_db.prepare("SELECT * FROM Map_data").all();
-    datasets.crosshair_data = data_db.prepare("SELECT * FROM Crosshair_data").all();
-    datasets.ships_gun_angles = data_db.prepare("SELECT * FROM Ships_gun_angles").all();
+    datasets.gun_stats = await db.collection("Gun_stats").find({}).toArray();
+    datasets.ammo_stats = await db.collection("Ammo_stats").find({}).toArray();
+    datasets.damage_stats = await db.collection("Damage_types").find({}).toArray();
+    datasets.tool_stats = await db.collection("Tool_stats").find({}).toArray();
+    datasets.component_stats = await db.collection("Component_stats").find({}).toArray();
+    datasets.ship_stats = await db.collection("Ship_Stats").find({}).toArray();
+    datasets.map_data = await db.collection("Map_data").find({}).toArray();
+    datasets.crosshair_data = await db.collection("Crosshair_data").find({}).toArray();
+    datasets.ships_gun_angles = await db.collection("Ships_gun_angles").find({}).toArray();
+    for (const [key, value] of Object.entries(datasets)) {
+        for (const v of datasets[key]) {
+            delete v._id;
+        }
+    }
+    console.log(datasets);
+
     res.status(200).json(datasets);
 });
 
