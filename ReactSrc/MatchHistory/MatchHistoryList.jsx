c7f14cac2f0f8ae813771251c1474b005f243dee
Jesper Ericsson <jjeessppeer@gmail.com>
2023-11-21 14:49:38 +0100

    button to load more match history

--- ReactSrc/MatchHistory/MatchHistoryList.jsx
+++ ReactSrc/MatchHistory/MatchHistoryList.jsx
@@ -4,23 +4,54 @@ import { Foldout } from '/React/MatchHistory/Foldout.js';
 export class MatchHistoryList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      matches: this.props.matches,
+      currentPage: 0,
+      loading: false
+    }
+    this.requestNextPage = this.requestNextPage.bind(this);
+  }
+
+  async requestNextPage() {
+    this.setState({
+      loading: true,
+    });
+    let response = await fetch('/request_matches', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          page: this.state.currentPage + 1, 
+          filters: this.props.searchFilters
+        })
+    });
+    let json = await response.json();
+    this.setState((oldState) => ({
+        loading: false,
+        currentPage: oldState.currentPage + 1,
+        matches: oldState.matches.concat(json.Matches)
+    }));
   }
 
   render() {
     const listElements = [];
-    for (let match of this.props.matches) {
-      console.log(match);
+    for (let match of this.state.matches) {
       listElements.push(<ListElement {...match}></ListElement>);
     }
     return (
-      <ul className="match-history-list">
-        {listElements}
-      </ul>
+      <div>
+        <ul className="match-history-list">
+          {listElements}
+        </ul>
+        <button class="load-more-matches-button" onClick={this.requestNextPage} disabled={this.state.loading}>Show Older</button>
+      </div>
+
     )
   }
 }
 
-export class ListElement extends React.Component {
+export class ListElement extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
@@ -31,18 +62,18 @@ export class ListElement extends React.Component {
   }
 
   toggleFoldout() {
-    console.log("TOGGLING");
     this.setState((oldState) => ({
       expanded: !oldState.expanded
     }));
   }
 
   render() {
+    console.log("ELEMENT RENDERING");
     return (
       <li className={`match-history-entry ${this.props.Winner == 0 ? "red-winner" : "blue-winner"}`}>
         <Overview {...this.props} toggleFoldout={this.toggleFoldout}></Overview>
         {this.state.expanded ? <Foldout {...this.props}></Foldout> : undefined}
       </li>
-    )
+    );
   }
 }
