66168948e5923078438134861a6c331ca5c7da07
Jesper Ericsson <jjeessppeer@gmail.com>
2023-12-02 14:10:35 +0100

    removed jquery usage

--- public/js/SpawnMaps.js
+++ public/js/SpawnMaps.js
@@ -4,14 +4,13 @@ async function initializeSpawns(){
     const mapFetch = await fetch("/maps/2/2/2");
     const mapList = await mapFetch.json();
     mapList.sort((a, b) => (a.Name > b.Name));
-    
+
     const select = document.getElementById("spawnMapSelect");
     for (const mapItem of mapList) {
         const opt = document.createElement('option');
         opt.textContent = mapItem.Name;
         opt.selected = mapItem.Name == "Canyon Ambush";
         opt.value = `2v2DM_${mapItem.Name.replaceAll(" ", "")}.jpg`;
-        console.log(opt.value);
         select.append(opt);
     }
     select.addEventListener('change', evt => {
--- public/js/utils.js
+++ public/js/utils.js
@@ -397,18 +397,18 @@ CanvasRenderingContext2D.prototype.zoomAround = function(x, y, factor){
     this.translate(-x, -y);
 }
 
-$.fn.inputFilter = function (inputFilter) {
-    return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function () {
-      if (inputFilter(this.value)) {
-        this.oldValue = this.value;
-        this.oldSelectionStart = this.selectionStart;
-        this.oldSelectionEnd = this.selectionEnd;
-      } else if (this.hasOwnProperty("oldValue")) {
-        this.value = this.oldValue;
-        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-      }
-    });
-};
+// $.fn.inputFilter = function (inputFilter) {
+//     return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function () {
+//       if (inputFilter(this.value)) {
+//         this.oldValue = this.value;
+//         this.oldSelectionStart = this.selectionStart;
+//         this.oldSelectionEnd = this.selectionEnd;
+//       } else if (this.hasOwnProperty("oldValue")) {
+//         this.value = this.oldValue;
+//         this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+//       }
+//     });
+// };
 
 
 function sanitizeHtml(str){
