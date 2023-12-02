6750dff03b0dc4157f74a56ae628ebc1c29574f4
Jesper Ericsson <jjeessppeer@gmail.com>
2023-12-02 13:31:52 +0100

    spawn points works with new database

---
+++ public/js/SpawnMaps.js
@@ -0,0 +1,25 @@
+
+
+async function initializeSpawns(){
+    const mapFetch = await fetch("/maps/2/2/2");
+    const mapList = await mapFetch.json();
+    mapList.sort((a, b) => (a.Name > b.Name));
+    
+    const select = document.getElementById("spawnMapSelect");
+    for (const mapItem of mapList) {
+        const opt = document.createElement('option');
+        opt.textContent = mapItem.Name;
+        opt.selected = mapItem.Name == "Canyon Ambush";
+        opt.value = `2v2DM_${mapItem.Name.replaceAll(" ", "")}.jpg`;
+        console.log(opt.value);
+        select.append(opt);
+    }
+    select.addEventListener('change', evt => {
+        console.log(evt.target.value)
+        const img = document.getElementById('spawnImage');
+        img.src = `/images/spawn-images/${evt.target.value}`;
+    });
+    
+}
+
+initializeSpawns();
