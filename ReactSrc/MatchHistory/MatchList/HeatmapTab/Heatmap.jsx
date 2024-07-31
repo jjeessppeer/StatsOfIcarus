c0a2285e7287ea06a0e520faf951ef46ffe4bc01
Jesper Ericsson <jjeessppeer@gmail.com>
2024-07-31 20:39:58 +0200

    heatmap and death points

--- ReactSrc/MatchHistory/MatchList/HeatmapTab/Heatmap.jsx
+++ ReactSrc/MatchHistory/MatchList/HeatmapTab/Heatmap.jsx
@@ -11,9 +11,7 @@ export class Heatmap extends React.Component {
 
   drawShipPaths(ctx, shipPaths) {
     const colors = ['red', 'orange', 'blue', 'cyan'];
-    
     for (const [objectId, shipPath] of Object.entries(shipPaths)) {
-      
       let lastP;
       let startNewPath = true;
       for (let i = 0; i < shipPath.length; i++) {
@@ -53,62 +51,42 @@ export class Heatmap extends React.Component {
         }
       }
     }
-      // let lastP;
-      // let startNewPath = true;
-      // for (let i = 0; i < shipPositions.Timestamp.length; i++) {
-      //   const p = positionToCanvasPixel(shipPositions.Position[i], this.props.mapItem, this.props.width);
-      //   if (startNewPath) {
-      //     ctx.beginPath();
-      //     ctx.moveTo(p[0][0], p[0][1]);
-      //     startNewPath = false;
-      //   }
-      //   else {
-      //     const xc = (lastP[0] + p[0]) / 2;
-      //     const yc = (lastP[1] + p[1]) / 2;
-      //     ctx.quadraticCurveTo(lastP[0], lastP[1], xc, yc);
-      //   }
-      //   if (shipPositions.Dead[i]) {
-      //     startNewPath = true;
-      //   }
-      //   lastP = p;
-
-      //   if ((startNewPath && i != 0) || i == shipPositions.Timestamp.length - 1) {
-      //     ctx.lineWidth = 4;
-      //     ctx.strokeStyle = 'white';
-      //     ctx.stroke();
-      //     ctx.lineWidth = 2;
-      //     ctx.strokeStyle = colors[shipPositions.TeamIdx * 2 + shipPositions.ShipIdx];
-      //     ctx.stroke();
-      //   }
-      // }
   }
 
-  drawDeathPoints(ctx, positionData) {
-    // for (const shipPositions of positionData) {
-    //   for (let i = 0; i < shipPositions.Timestamp.length; i++) {
-    //     if (!shipPositions.Dead[i]) continue;
-    //     const p = positionToCanvasPixel(shipPositions.Position[i], this.props.mapItem, this.props.width);
-    //     ctx.beginPath();
-    //     ctx.fillStyle = 'white';
-    //     ctx.arc(p[0], p[1], 10, 0, 2 * Math.PI);
-    //     ctx.fill();
-
-    //     ctx.fillStyle = 'black';
-    //     ctx.beginPath();
-    //     ctx.arc(p[0], p[1], 8, 0, 2 * Math.PI);
-    //     ctx.fill();
-    //   }
-    // }
+  drawDeathPoints(ctx, shipPaths) {
+    for (const [objectId, shipPath] of Object.entries(shipPaths)) {
+      let lastP;
+      let startNewPath = true;
+      for (let i = 0; i < shipPath.length; i++) {
+        const position = shipPath[i].position;
+        if (!shipPath[i].alive && !shipPath[i-1].alive) {
+          // Ship died this update.
+          const p = positionToCanvasPixel([position.x, 0, position.y], this.props.mapItem, this.props.width);
+          ctx.beginPath();
+          ctx.fillStyle = 'white';
+          ctx.arc(p[0], p[1], 5, 0, 2 * Math.PI);
+          ctx.fill();
+  
+          ctx.fillStyle = 'black';
+          ctx.beginPath();
+          ctx.arc(p[0], p[1], 3, 0, 2 * Math.PI);
+          ctx.fill();
+        }
+      }
+    }
   }
 
-  drawHeatmap(canvas, positionData) {
+  drawHeatmap(canvas, shipPaths) {
     const heatmapData = [];
-    // for (const shipPositions of positionData) {
-    //   for (let i = 0; i < shipPositions.Timestamp.length; i++) {
-    //     const p = positionToCanvasPixel(shipPositions.Position[i], this.props.mapItem, this.props.width, this.props.heatmapStrength);
-    //     heatmapData.push(p);
-    //   }
-    // }
+    for (const [objectId, shipPath] of Object.entries(shipPaths)) {
+      for (let i = 0; i < shipPath.length; i++) {
+        const position = shipPath[i].position;
+        const alive = shipPath[i].alive;
+        if (!alive) continue;
+        const p = positionToCanvasPixel([position.x, 0, position.y], this.props.mapItem, this.props.width, this.props.heatmapStrength);
+        heatmapData.push(p);
+      }
+    }
     const heat = simpleheat(canvas);
     heat.gradient({
       0.08: 'rgba(0, 0, 255, 1)',
@@ -120,7 +98,6 @@ export class Heatmap extends React.Component {
     heat.radius(this.props.heatmapRadius, this.props.heatmapBlur);
     heat.data(heatmapData);
     heat.draw(0);
-
   }
 
   redrawCanvas = () => {
@@ -129,16 +106,15 @@ export class Heatmap extends React.Component {
     ctx.globalCompositeOperation = 'source-over';
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // if (this.props.canvasType == 'heatmap') {
-    //   ctx.save();
-    //   this.drawHeatmap(canvas, this.props.shipPositions);
-    //   ctx.restore();
-    // }
-    // else if (this.props.canvasType == 'paths') {
-    //   this.drawShipPaths(ctx, this.props.shipPositions);
-    //   this.drawDeathPoints(ctx, this.props.shipPositions);  
-    // } 
-    this.drawShipPaths(ctx, this.props.shipPaths);
+    if (this.props.canvasType == 'heatmap') {
+      ctx.save();
+      this.drawHeatmap(canvas, this.props.shipPaths);
+      ctx.restore();
+    }
+    else if (this.props.canvasType == 'paths') {
+      this.drawShipPaths(ctx, this.props.shipPaths);
+      this.drawDeathPoints(ctx, this.props.shipPaths);  
+    } 
   }
 
   componentDidUpdate() {
